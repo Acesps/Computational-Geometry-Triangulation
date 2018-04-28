340f0dbb6012e268e49d29716a5b2225dc38a918
Acesps <000sps97@gmail.com>
2018-04-29 01:09:56 +0530

    header files added

---
+++ include/List.h
@@ -0,0 +1,18 @@
+#ifndef LIST_H
+#define LIST_H
+#include "Point.h"
+
+using namespace std;
+class List
+{
+    public:
+        List();
+
+        virtual ~List();
+
+    protected:
+
+    private:
+};
+
+#endif // LIST_H
